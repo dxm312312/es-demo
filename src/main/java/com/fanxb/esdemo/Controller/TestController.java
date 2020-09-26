843258a54c4d56eb750af984be4c8de6a108a852
dxm112234 <dxm112234@126.com>
2020-09-26 21:10:32 +0800

    es-demo

--- src/main/java/com/fanxb/esdemo/Controller/TestController.java
+++ src/main/java/com/fanxb/esdemo/Controller/TestController.java
@@ -1,21 +1,32 @@
 package com.fanxb.esdemo.Controller;
 
+import com.alibaba.fastjson.JSON;
 import com.fanxb.esdemo.entity.Product;
+import com.fanxb.esdemo.repository.ProductRepository;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.query.IndexQuery;
-import org.springframework.data.elasticsearch.core.query.IndexQueryBuilder;
-import org.springframework.data.elasticsearch.core.query.UpdateQuery;
+import org.springframework.data.elasticsearch.core.SearchHit;
+import org.springframework.data.elasticsearch.core.SearchHits;
+import org.springframework.data.elasticsearch.core.mapping.IndexCoordinates;
+import org.springframework.data.elasticsearch.core.query.*;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/")
@@ -23,6 +34,9 @@ public class TestController {
     @Autowired
     ElasticsearchOperations elasticsearchTemplate;
    //RestHighLevelClient elasticsearchClient;
+
+    @Autowired
+    ProductRepository productRepository;
 
     @PostMapping("/save")
     public String save(@RequestBody Product product) throws IOException {
@@ -54,6 +68,34 @@ public class TestController {
         elasticsearchTemplate.delete(product);
         //
         return "success";
+    }
+
+    @PostMapping("/condetionSearch")
+    public String condetionSearch(@RequestBody Product product) throws IOException {
+        //withExcludes() 剔除的字段集合   withIncludes 要查的字段集合
+        SourceFilter filter = new FetchSourceFilterBuilder().withIncludes("name","sellPoint").build();
+        NativeSearchQueryBuilder searchQuery = new NativeSearchQueryBuilder().withSourceFilter(filter);
+        BoolQueryBuilder builder = QueryBuilders.boolQuery();
+
+        builder.must(QueryBuilders.matchQuery("id",product.getId()));
+
+
+        searchQuery.withQuery(builder);
+        /*Pageable pageable = PageRequest.of(0,10);
+        searchQuery.withPageable(pageable);*/
+
+        SearchHits<Product> search = elasticsearchTemplate.search(searchQuery.build(), Product.class);
+
+        IndexCoordinates index = IndexCoordinates.of("product");
+        List<Product> products = elasticsearchTemplate.queryForList(searchQuery.build(), Product.class, index);
+
+        return JSON.toJSONString(products);
+    }
+
+    @PostMapping("/searchByCid")
+    public String searchByCid(@RequestBody Product product) throws IOException {
+        List<Product> pList = productRepository.findByCid(product.getCid());
+        return JSON.toJSONString(pList);
     }
 
 }
--- src/main/java/com/fanxb/esdemo/repository/ProductRepository.java
+++ src/main/java/com/fanxb/esdemo/repository/ProductRepository.java
@@ -2,7 +2,11 @@ package com.fanxb.esdemo.repository;
 
 import com.fanxb.esdemo.entity.Product;
 import org.springframework.data.repository.Repository;
+import org.springframework.stereotype.Component;
 
+import java.util.List;
+
+@Component
 public interface ProductRepository extends Repository<Product,String> {
-
+    List<Product> findByCid(Integer cid);
 }
